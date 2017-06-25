11d31bcf52cad8881049c6ca6fd5e02c5146821b
buzzxu <xiang.leau@gmail.com>
2017-06-26 00:11:42 +0800

    json添加 ‘omitempty’

--- config.go
+++ config.go
@@ -6,13 +6,13 @@ type (
 	// Error 异常结构
 	Error struct {
 		Code    int         `json:"code"`
-		Key     string      `json:"error"`
+		Key     string      `json:"error,omitempty"`
 		Message interface{} `json:"message"`
 	}
 	// Result 返回结果
 	Result struct {
 		Code int         `json:"code"`
-		Data interface{} `json:"data"`
+		Data interface{} `json:"data,omitempty"`
 	}
 )
 
--- model.go
+++ model.go
@@ -4,23 +4,23 @@ type (
 	// User 用户基础信息
 	User struct {
 		ID         int    `json:"id" gorm:"primary_key"`
-		Version    int    `gorm:"column:_version"`
-		ParentID   int    `json:"parentId" gorm:"column:parent_id"`
-		UserName   string `json:"userName" gorm:"column:user_name"`
-		RealName   string `json:"realName" gorm:"column:real_name"`
-		Password   string `json:"password" gorm:"column:password"`
-		Type       int8   `json:"type"`
-		Sex        int8   `json:"sex"`
-		Avatar     string `json:"avatar"`
-		City       string `json:"city"`
-		Province   string `json:"province"`
-		Country    string `json:"country"`
-		Email      string `json:"email"`
-		Mobile     string `json:"mobile"`
-		RegTime    int64  `json:"regTime"`
-		UpgradedAt int64  `json:"upgradedAt" gorm:"column:upgraded_at"`
-		CreatedAt  int64  `json:"createdAt" gorm:"column:created_at"`
-		UpdatedAt  int64  `json:"updatedAt" gorm:"column:updated_at"`
+		Version    int    `json:"version,omitempty"gorm:"column:_version"`
+		ParentID   int    `json:"parentId,omitempty" gorm:"column:parent_id"`
+		UserName   string `json:"userName,omitempty" gorm:"column:user_name"`
+		RealName   string `json:"realName,omitempty" gorm:"column:real_name"`
+		Password   string `json:"password,omitempty" gorm:"column:password"`
+		Type       int8   `json:"type,omitempty"`
+		Sex        int8   `json:"sex,omitempty"`
+		Avatar     string `json:"avatar,omitempty"`
+		City       string `json:"city,omitempty"`
+		Province   string `json:"province,omitempty"`
+		Country    string `json:"country,omitempty"`
+		Email      string `json:"email,omitempty"`
+		Mobile     string `json:"mobile,omitempty"`
+		RegTime    int64  `json:"regTime,omitempty"`
+		UpgradedAt int64  `json:"upgradedAt,omitempty" gorm:"column:upgraded_at"`
+		CreatedAt  int64  `json:"createdAt,omitempty" gorm:"column:created_at"`
+		UpdatedAt  int64  `json:"updatedAt,omitempty" gorm:"column:updated_at"`
 	}
 	// UserWx 用户微信信息
 	UserWx struct {
