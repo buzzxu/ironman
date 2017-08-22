b5339f98d3e5ed67a6a2a8eebf006f3eb4b3b5e9
buzzxu <xiang.leau@gmail.com>
2017-08-22 21:39:32 +0800

    添加用户微信信息

--- model.go
+++ model.go
@@ -6,33 +6,32 @@ type (
 		ID         int    `json:"id" gorm:"primary_key"`
 		Version    int    `json:"version,omitempty"gorm:"column:_version"`
 		ParentID   int    `json:"parentId,omitempty" gorm:"column:parent_id"`
-		NickName string `json:"nick_name" gorm:"column:nick_name"`
+		NickName   string `json:"nick_name" gorm:"column:nick_name"`
 		UserName   string `json:"userName,omitempty" gorm:"column:user_name"`
 		RealName   string `json:"realName,omitempty" gorm:"column:real_name"`
 		Password   string `json:"password,omitempty" gorm:"column:password"`
 		Type       int8   `json:"type,omitempty"`
-		Sex        int   `json:"sex,omitempty"`
+		Sex        int    `json:"sex,omitempty"`
 		Avatar     string `json:"avatar,omitempty"`
 		City       string `json:"city,omitempty"`
 		Province   string `json:"province,omitempty"`
 		Country    string `json:"country,omitempty"`
 		Email      string `json:"email,omitempty"`
 		Mobile     string `json:"mobile,omitempty"`
-		Score int `json:"score"`
+		Score      int    `json:"score"`
 		RegTime    int64  `json:"regTime,omitempty"`
 		UpgradedAt int64  `json:"upgradedAt,omitempty" gorm:"column:upgraded_at"`
 		CreatedAt  int64  `json:"createdAt,omitempty" gorm:"column:created_at"`
 		UpdatedAt  int64  `json:"updatedAt,omitempty" gorm:"column:updated_at"`
 	}
-	// UserWx 用户微信信息
-	UserWx struct {
-		ID      int    `json:"id" gorm:"primary_key"`
-		UnionID string `json:"unionId"`
-		OpenID  string `json:"openId"`
+	// UserWechat 用户微信信息
+	UserWechat struct {
+		ID        int    `json:"id" gorm:"primary_key"`
+		UnionID   string `json:"unionId" gorm:"column:unionid"`
+		OpenID    string `json:"openId" gorm:"column:openid"`
+		CreatedAt int64  `json:"created_at" gorm:"column:created_at"`
+		UpdatedAt int64  `json:"updated_at" gorm:"column:updated_at"`
 	}
 	// Map 类型
 	Map map[string]interface{}
 )
-
-// TableName user_wx
-func (u UserWx) TableName() string { return "t_user_wx" }
