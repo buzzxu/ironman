77212424a48f45aa5a94096754250c930f986b47
buzzxu <xiang.leau@gmail.com>
2020-09-10 18:48:39 +0800

    redis配置调整

--- redis.go
+++ redis.go
@@ -12,21 +12,24 @@ var Redis *redis.Client
 
 // RedisConnect Redis连接
 func RedisConnect() {
-	var password = ""
-	var poolSize = 10
-	if len(conf.ServerConf.Redis.Password) > 0 {
-		password = conf.ServerConf.Redis.Password
+	if conf.ServerConf.Redis.Addr != "" {
+		var password = ""
+		var poolSize = conf.ServerConf.MaxProc * 5
+		if len(conf.ServerConf.Redis.Password) > 0 && conf.ServerConf.Redis.Password != "none" {
+			password = conf.ServerConf.Redis.Password
+		}
+		if conf.ServerConf.Redis.PoolSize > 0 {
+			poolSize = conf.ServerConf.Redis.PoolSize
+		}
+		Redis = redis.NewClient(&redis.Options{
+			Addr:     conf.ServerConf.Redis.Addr,
+			Password: password,                 // no password set
+			DB:       conf.ServerConf.Redis.DB, // use default DB
+			PoolSize: poolSize,
+		})
+		pong, err := Redis.Ping().Result()
+		fmt.Println(pong, err)
+	} else {
+		fmt.Printf("Redis未配置,无需连接")
 	}
-	if conf.ServerConf.Redis.PoolSize > 0 {
-		poolSize = conf.ServerConf.Redis.PoolSize
-	}
-	Redis = redis.NewClient(&redis.Options{
-		Addr:     conf.ServerConf.Redis.Addr,
-		Password: password,                 // no password set
-		DB:       conf.ServerConf.Redis.DB, // use default DB
-		PoolSize: poolSize,
-	})
-
-	pong, err := Redis.Ping().Result()
-	fmt.Println(pong, err)
 }
